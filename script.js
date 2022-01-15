7d9ff5bf6ad9a33aea820c3b59b43e526fd6b635
lari-ssa23 <larissa.isabel@unesp.br>
2022-01-15 00:27:53 -0300

    primeiro desafio

---
+++ script.js
@@ -0,0 +1,10 @@
+
+let name = "Larissa";
+console.log(name);
+
+let numero = 22;
+console.log(numero);
+
+let anoresultado = 2022 - numero;
+console.log(anoresultado);
+const somaDuas = numero + anoresultado;
